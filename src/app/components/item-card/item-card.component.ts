718bddf16c47a5bc95988f3f03321484759f78d8
Cupid <hien.lm1452@sinhvien.hoasen.edu.vn>
2023-07-27 23:49:49 +0700

    A 3

--- src/app/components/item-card/item-card.component.ts
+++ src/app/components/item-card/item-card.component.ts
@@ -1,5 +1,8 @@
 import { Item } from 'src/app/models/item.model';
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/cart.service';
+
 
 @Component({
   selector: 'app-item-card',
@@ -7,7 +10,21 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./item-card.component.scss']
 })
 export class ItemCardComponent {
+
   @Input()
   item!: Item;
+   itemInCart: Item[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit(): void {}
+
+  addToCart(product: Item) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 }
  
