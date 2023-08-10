f53d869437b17d59ec13af6aa6e2c08fb395736f
Cupid <hien.lm1452@sinhvien.hoasen.edu.vn>
2023-08-11 01:15:06 +0700

    A 22

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,22 +1,18 @@
+
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Firestore, collection, collectionSnapshots } from '@angular/fire/firestore';
 import { Chat } from '../models/chat.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-  listChats: Chat[] = [];
-  itemsCollection = collection(this.firestore, 'chat');
-  constructor(private firestore: Firestore) {
-    this.getData();
-   }
-   getData() {
-     collectionSnapshots(this.itemsCollection).subscribe((snapshot) => {
-       let result = snapshot.map((doc) => doc.data());
-       this.listChats = result as Chat[];
-    });
-   }
+  serverAPI = 'http://localhost:1821/chat';
+
+  Chat$ = new Subject<Chat[]>();
+
+
 
 }
